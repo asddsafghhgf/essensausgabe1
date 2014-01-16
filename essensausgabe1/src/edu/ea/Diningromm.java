112b2bc5264533baee5867f7f1e24c8e89fd2bb3
asddsafghhgf <drithust@wegwerfemail.de>
2014-01-16 07:13:02 +0100

    SOme Classes

---
+++ essensausgabe1/src/edu/ea/Diningromm.java
@@ -0,0 +1,13 @@
+package edu.ea;
+
+public class Diningromm {
+
+	private int amountOfGuests = 0;
+	private int maxAmountOfGuests = 24;
+	private int waiters = 2;
+	private int amountOfTables = 4;
+	private Guest[] tabel0 = new Guest[8];
+	private int[] tabels = {8,4,6,6};
+	 
+	
+}
---
+++ essensausgabe1/src/edu/ea/Kitchen.java
@@ -0,0 +1,5 @@
+package edu.ea;
+
+public class Kitchen {
+
+}
